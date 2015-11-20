6c3c5515e77782310590f181a5d1539ddc75da7b
anil <anil.gautam@gmail.com>
2015-11-20 15:23:40 -0500

    added support to send useragent

--- woocommerce-payfort.php
+++ woocommerce-payfort.php
@@ -277,5 +277,27 @@ endif;
         $methods[] = 'WC_Gateway_Payfort';
         return $methods;
     }
+    
+    /**
+     * Get woocommerce version
+     * @return type
+     */
+    function wpbo_get_woo_version_number() {
+        // If get_plugins() isn't available, require it
+        if (!function_exists('get_plugins'))
+            require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+        // Create the plugins folder and file variables
+        $plugin_folder = get_plugins('/' . 'woocommerce');
+        $plugin_file = 'woocommerce.php';
+
+        // If the plugin version number is set, return it 
+        if (isset($plugin_folder[$plugin_file]['Version'])) {
+            return $plugin_folder[$plugin_file]['Version'];
+        } else {
+            // Otherwise return null
+            return NULL;
+        }
+    }
     add_filter('woocommerce_payment_gateways', 'add_payfort_gateway');
 }
