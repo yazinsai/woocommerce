abb0b22cd3cfe0f683e9a0dfd1724438172ae783
anil gautam <anil.gautamm@gmail.com>
2015-11-27 18:55:15 +0530

    updated payfort start

--- vendor/payfort/start/src/Start.php
+++ vendor/payfort/start/src/Start.php
@@ -14,13 +14,21 @@ class Start
   * Client version
   * @var string
   */
-  const VERSION = '0.0.5';
+  const VERSION = '0.0.8';
 
   /**
   * Current API key
   * @var string
   */
   protected static $apiKey;
+  
+  /**
+  * ShoppingCart name and version
+  * Our plugin version
+  * “WooCommerce 4.2.2 / Start Plugin 0.1.2”
+  * @var string
+  */
+  protected static $userAgent = "";
 
   /**
   * API Server URL
@@ -65,6 +73,25 @@ class Start
   public static function getApiKey()
   {
     return self::$apiKey;
+  }
+  /**
+  * sets API Key
+  *
+  * @param string $userAgent UserAgent
+  */
+  public static function setUserAgent($userAgent)
+  {
+    self::$userAgent = $userAgent;
+  }
+
+  /**
+  * returns current set UserAgent
+  *
+  * @return string UserAgent
+  */
+  public static function getUserAgent()
+  {
+    return self::$userAgent;
   }
 
   /**
