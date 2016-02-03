04e424cc544fa0047c4235cd9b90a27c2e54826d
Pavel Gabriel <alovak@gmail.com>
2016-02-03 14:09:00 +0300

    update to 0.1.7

--- woocommerce-payfort.php
+++ woocommerce-payfort.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Payfort (Start)
 Description: Payfort makes it really easy to start accepting online payments (credit &amp; debit cards) in the Middle East. Sign up is instant, at https://start.payfort.com/
-Version: 0.1.6
+Version: 0.1.7
 Plugin URI: https://start.payfort.com
 Author: Payfort
 Author URI: https://start.payfort.com
