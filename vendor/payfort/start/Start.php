a07b484756bb7ce349bd6ce9ed6286234c35ec5f
Yazin <yazinsai@users.noreply.github.com>
2016-02-07 11:47:27 +0400

    Update Start.php

--- vendor/payfort/start/Start.php
+++ vendor/payfort/start/Start.php
@@ -1,15 +1,3 @@
 <?php
 
-//File to use when using lib without composer.
-require_once 'src/Start.php';
-require_once 'src/Start/Net/Curl.php';
-require_once 'src/Start/Net/Stream.php';
-require_once 'src/Start/Request.php';
-require_once 'src/Start/Charge.php';
-require_once 'src/Start/Error.php';
-require_once 'src/Start/Customer.php';
-require_once 'src/Start/Error/Authentication.php';
-require_once 'src/Start/Error/Banking.php';
-require_once 'src/Start/Error/Processing.php';
-require_once 'src/Start/Error/Request.php';
-require_once 'src/Start/Error/SSLError.php';
+# broken now :)
